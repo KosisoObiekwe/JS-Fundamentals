6999e68a9abf382d7a4c76eaae61bec206d6f7d6
Obiekwe Kosi <lol4u2468@gmail.com>
2025-05-26 01:20:38 +0100

    FIXED: 9-add.js now passes all test cases

--- 9-add.js
+++ 9-add.js
@@ -1,9 +1,10 @@
+#!/usr/bin/node
 function add(a, b) {
-  a = isNaN(a) ? 0 : a;
-  b = isNaN(b) ? 0 : b;
+  a = typeof a === 'number' ? a : 0;
+  b = typeof b === 'number' ? b : 0;
   return a + b;
 }
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
+const num1 = process.argv[2] ? parseInt(process.argv[2]) : 0;
+const num2 = process.argv[3] ? parseInt(process.argv[3]) : 0;
 console.log(add(num1, num2));
