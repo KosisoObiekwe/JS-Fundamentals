3c5109ad084f4bfd06c966c6a6ac41c20727ade0
Obiekwe Kosi <lol4u2468@gmail.com>
2025-05-26 10:31:45 +0100

    Update 9-add.js
    "Fix: handle missing arguments by printing NaN"

--- 9-add.js
+++ 9-add.js
@@ -1,10 +1,14 @@
-#!/usr/bin/node
+// 9-add.js
+
 function add(a, b) {
-  a = typeof a === 'number' ? a : 0;
-  b = typeof b === 'number' ? b : 0;
   return a + b;
 }
 
-const num1 = process.argv[2] ? parseInt(process.argv[2]) : 0;
-const num2 = process.argv[3] ? parseInt(process.argv[3]) : 0;
-console.log(add(num1, num2));
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[3]);
+
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.log('NaN');
+} else {
+  console.log(add(arg1, arg2));
+}
