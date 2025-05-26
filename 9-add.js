197e3ec8984ab8e2291464ffdb54107da4ff58cb
Obiekwe Kosi <lol4u2468@gmail.com>
2025-05-26 01:16:06 +0100

    Fixed 9-add.js to handle all test cases

--- 9-add.js
+++ 9-add.js
@@ -1,2 +1,9 @@
-function add(a, b) { return a + b; }
-console.log(add(parseInt(process.argv[2]) || 0, parseInt(process.argv[3]) || 0));
+function add(a, b) {
+  a = isNaN(a) ? 0 : a;
+  b = isNaN(b) ? 0 : b;
+  return a + b;
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+console.log(add(num1, num2));
